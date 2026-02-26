5ceead094c0714cf5fdbb5eeddfa1fd466a7b3c3
klentaetnaglazah <felixxgg42@gmail.com>
2026-02-26 16:11:33 +0400

    correction

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import fastify from 'fastify'
+
+const app = fastify()
+const port = 3000
+
+app.get('/users', (req, res) => {
+  res.send('GET /users')
+})
+
+app.post('/users', (req, res) => {
+  res.send('POST /users')
+})
+
+app.listen({ port }, () => {
+  console.log(`Example app listening on port ${port}`)
+})
