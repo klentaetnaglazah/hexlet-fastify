58067f1c08e7722e65b85993b8c1061b87062a12
klentaetnaglazah <felixxgg42@gmail.com>
2026-02-26 20:37:36 +0400

    added user and post id handler

--- src/index.js
+++ src/index.js
@@ -3,17 +3,21 @@ import fastify from 'fastify'
 const app = fastify()
 const port = 3000
 
-app.get('/hello', (req, res) => {
-  const name = req.query.name || 'World'
-  res.send(`Hello, ${name}!`)
-})
-
 app.get('/users', (req, res) => {
   res.send('GET /users')
 })
 
 app.post('/users', (req, res) => {
   res.send('POST /users')
+})
+
+app.get('/users/:id/post/:postId', (req, res) => {
+  res.send(`User ID: ${req.params.id}; Post ID: ${req.params.postId}`)
+})
+
+app.get('/hello', (req, res) => {
+  const name = req.query.name || 'World'
+  res.send(`Hello, ${name}!`)
 })
 
 app.listen({ port }, () => {
